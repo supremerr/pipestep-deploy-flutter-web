ad3c110628a267dbf5966de325f972f406ceb332
Vinicius Sousa <csousa.vinicius@gmail.com>
2021-03-15 15:24:56 -0300

    [Flutter] fix: path and working dir

--- src/br/com/zup/deploy/DeployDevWeb.groovy
+++ src/br/com/zup/deploy/DeployDevWeb.groovy
@@ -12,9 +12,10 @@ class DeployDevWeb {
             jenkins.node(jenkins.POD_LABEL){
                 jenkins.container('jnlp'){
                     try{
-                        jenkins.s3Upload(bucket:"http://sample-app-flutter.s3-website-sa-east-1.amazonaws.com", path:'/build/web', includePathPattern:'**/*', workingDir:'/build/web', excludePathPattern:'**/*.svg,**/*.jpg')
+                        jenkins.s3Upload(bucket:"http://sample-app-flutter.s3-website-sa-east-1.amazonaws.com", path:'', includePathPattern:'**/*', workingDir:'build/web', excludePathPattern:'**/*.svg,**/*.jpg')
                     }
                     catch(Exception e){
+                        jenkins.echo e
                         jenkins.unstable("An error occured during deploy step. Please, verify the logs.")
                     }
                 }
