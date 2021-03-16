69aab7fae3d459c6063505afb99f28a9865d60c3
Vinicius Sousa <csousa.vinicius@gmail.com>
2021-03-16 16:01:35 -0300

    [Flutter] fix: bucket name value with env variable

--- src/br/com/zup/deploy/DeployDevWeb.groovy
+++ src/br/com/zup/deploy/DeployDevWeb.groovy
@@ -2,6 +2,8 @@ package br.com.zup.deploy
 
 class DeployDevWeb {
     def call (jenkins) {
+        def bucketName = jenkins.env.JOB_NAME.toLowerCase()
+        
         jenkins.podTemplate(
             yamlMergeStrategy: jenkins.merge(),
             workspaceVolume: jenkins.persistentVolumeClaimWorkspaceVolume(
@@ -13,7 +15,7 @@ class DeployDevWeb {
                 jenkins.container('jnlp'){
                     try{
                         jenkins.withAWS(credentials: 'aws-credential') {
-                            jenkins.s3Upload(bucket:"sample-app-flutter", path:'', includePathPattern:'**/*', workingDir:'build/web', excludePathPattern:'**/*.svg,**/*.jpg')
+                            jenkins.s3Upload(bucket:"${bucketName}.dev.iupp.io", path:'', includePathPattern:'**/*', workingDir:'build/web', excludePathPattern:'**/*.svg,**/*.jpg')
                         }
                     }
                     catch(Exception e){
